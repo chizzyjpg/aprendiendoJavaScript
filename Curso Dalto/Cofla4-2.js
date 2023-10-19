0fa5080923ad5e65b2cd3e2113ef5caff0e9f89c
Chizzyjpg <chizzyjpg@gmail.com>
2023-10-19 17:27:26 -0300

    gassd

--- Curso Dalto/Cofla4-2.js
+++ Curso Dalto/Cofla4-2.js
@@ -3,25 +3,75 @@ const obtenerInformacion = (materia) => {
     fisica: ["bonancea", "jose", "ivan", "carlos", "cofla"],
     programacion: ["Dalto", "jose", "carlos"],
     logica: ["Eduardo", "jose", "pedro", "ivan", "carlos"],
-    quimica: ["rodriguez", "jose", "pedro", "ivan", "carlos"],
+    quimica: ["rodriguez", "jose", "cofla", "ivan", "carlos"],
   };
-  if (materias[materia] != undefined) {
-    return [materias[materia], materia];
+  if (materias[materia] !== undefined) {
+    return [materias[materia], materia, materias];
   } else {
-    return false;
+    return materias;
   }
 };
 
-let info = obtenerInformacion(prompt("Ingrece materia"));
+const mostrarInformacion = (materia) => {
+  let info = obtenerInformacion(materia);
+  if (info !== false) {
+    let profe = info[0][0];
+    let alumnos = info[0];
+    alumnos.shift();
 
-if (info != false) {
-  let profe = info[0][0];
-  document.write(
-    `el profesor de <b style = 'color:red'>${info[1]}</b> es: <b style = 'color:red'>${profe}</b> y sus alumnos son: <b style = 'color:red'>${info[0][1]}</b>`
-  );
-  let i = 2;
-  while (info[0][i] != undefined) {
-    document.write(`, <b style = 'color:red'>${info[0][i]}</b>`);
-    i++;
+    document.write(
+      `el profesor de <b style = 'color:red'>${info[1]}</b> es: <b style = 'color:red'>${profe}</b> y sus alumnos son: <b style = 'color:red'>${alumnos}</b> <br>`
+    );
   }
-}
+};
+
+const cantidadDeMaterias = (alumno) => {
+  let cantidadTotal = 0;
+  let info = obtenerInformacion();
+  for (inf in info) {
+    if (info[inf].includes(alumno)) {
+      cantidadTotal++;
+    }
+  }
+  return cantidadTotal;
+};
+
+const enQueMateriasEsta = (alumno) => {
+  let i = 0;
+  let info = obtenerInformacion();
+  let materias = [];
+  let profesor = [];
+  for (materia in info) {
+    if (info[materia].includes(alumno)) {
+      materias[i] = materia;
+      profesor[i] = info[materia][0];
+      i++;
+    }
+  }
+  let cantidad = i;
+  document.write(
+    alumno +
+      " esta en la materia: " +
+      materias[0] +
+      " con su profesor " +
+      profesor[0]
+  );
+  for (i = 1; i < cantidad; i++) {
+    if (cantidad - 1 === i) {
+      document.write(` y ${materias[i]} con su profesor ${profesor[i]}`);
+    } else {
+      document.write(`, ${materias[i]} con su profesor ${profesor[i]}`);
+    }
+  }
+};
+
+mostrarInformacion("fisica");
+mostrarInformacion("programacion");
+mostrarInformacion("logica");
+mostrarInformacion("quimica");
+
+document.write("<br> esta en: " + cantidadDeMaterias("jose") + " materias ");
+
+document.write("<br>");
+
+enQueMateriasEsta("jose");
